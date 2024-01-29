07dccefaa817fa3b19cff43103b6152024038b93
Jayathu <jayathu.shehan.2002@gmail.com>
2024-01-29 13:00:05 +0530

    changed

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'My Group is SD12',
+              'My Group is SD12 this is new one',
             ),
             Text(
               '$_counter',
