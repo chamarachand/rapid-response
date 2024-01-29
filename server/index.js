c5d3b8c29093ad8ea9b9ceed43209138686ede91
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-01-29 07:29:07 +0530

    [Server] Create port connection

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+// Database connection
+
+// Middleware
+
+// Connecting to the port
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening to port ${port}..`));
