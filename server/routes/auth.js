1d943511639b1aba376ab755eb093c5d505eb3ca
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-02-05 18:23:37 +0530

    [Server] Create post route handler to autheticate user login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -14,6 +14,19 @@ router.post("/", async (req, res) => {
       Civilian.findOne({ username: req.body.username }),
       FirstResponder.findOne({ username: req.body.username }),
     ]);
+
+    if (!civilian && !firstResponder)
+      return res.status(401).send("Invalid username or password");
+
+    const user = civilian || firstResponder;
+    const userType = civilian ? "civilian" : "first responder";
+
+    if (!correctPassword(req.body.password, user.password))
+      return res.status(401).send("Invalid username or password");
+
+    return res
+      .status(200)
+      .send({ message: "Login successful", userType: userType });
   } catch (error) {
     res.status(500).send("Internal server error");
   }
