56b4da3a45128014612b8bf61671704795c74090
Jayathu <jayathu.shehan.2002@gmail.com>
2024-02-06 18:03:20 +0530

    developed SOS_page

--- client/lib/pages/SOS_page.dart
+++ client/lib/pages/SOS_page.dart
@@ -10,10 +10,52 @@ class SOSpage extends StatelessWidget {
         title: Text(
           'SOS',
           style: TextStyle(
-              color: Colors.black, fontSize: 30, fontWeight: FontWeight.bold),
+              color: Color.fromARGB(255, 70, 70, 70),
+              fontSize: 30,
+              fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 55,
+              backgroundColor: Color.fromARGB(255, 235, 235, 235),
+              child: Icon(Icons.camera_alt, size: 60, color: Colors.grey[700]),
+            ),
+            SizedBox(height: 30),
+            CircleAvatar(
+              radius: 55,
+              backgroundColor: Color.fromARGB(255, 235, 235, 235),
+              child: Icon(Icons.mic, size: 60, color: Colors.grey[700]),
+            ),
+            SizedBox(height: 30),
+            DropdownButton<String>(
+              value: "Emergency Type",
+              items: <String>["Emergency Type", "Fire", "Medical", "Police"]
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                    value: value, child: Text(value));
+              }).toList(),
+              onChanged: (String? value) {},
+//                onChanged :newMethod
+            ),
+            SizedBox(height: 30),
+            TextField(
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.pink[100],
+                    hintText: "Not in the list? Type Here",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))))),
+            ElevatedButton(onPressed: null, child: new Text("Send SOS"))
+          ],
+        ),
+      ),
     );
   }
+
+  void newMethod(String newValue) {}
 }
