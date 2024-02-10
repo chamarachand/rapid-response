b4965ea5b24351e78ff19a4082834bb7e946dcac
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-02-10 14:56:24 +0530

    [Client] Create sample login page

--- client/lib/pages/login_screen.dart
+++ client/lib/pages/login_screen.dart
@@ -5,6 +5,8 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(child: Text("This is login page")),
+    );
   }
 }
