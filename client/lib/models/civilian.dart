7b5648bbb5fe03ee447f10b4410e5f1667cbc020
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-02-11 11:16:47 +0530

    [Client] Create Civilian model class

---
+++ client/lib/models/civilian.dart
@@ -0,0 +1,23 @@
+class Civilian {
+  String firstName;
+  String lastName;
+  String nicNumber;
+  String gender;
+  DateTime dateOfBirth;
+  String mobileNumber;
+  String email;
+  String username;
+  String password;
+
+  Civilian({
+    required this.firstName,
+    required this.lastName,
+    required this.nicNumber,
+    required this.gender,
+    required this.dateOfBirth,
+    required this.mobileNumber,
+    required this.email,
+    required this.username,
+    required this.password,
+  });
+}
