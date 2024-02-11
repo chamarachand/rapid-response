1327e7224dd823074a5ae5c5686ef105359551af
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-02-11 15:02:40 +0530

    [Client] Create main input fields in register page 4, username, password and confirm password

--- client/lib/pages/register_screen_4.dart
+++ client/lib/pages/register_screen_4.dart
@@ -8,6 +8,10 @@ class RegisterPage4 extends StatefulWidget {
 }
 
 class _RegisterScreen4State extends State<RegisterPage4> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _repasswordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,12 +21,48 @@ class _RegisterScreen4State extends State<RegisterPage4> {
           Padding(
             padding: EdgeInsets.only(left: 8),
             child: Icon(
-              Icons.app_registration,
+              Icons.person,
               size: 32,
             ),
           )
         ]),
       ),
+      body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          child: TextFormField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                  labelText: "Select your username",
+                  border: OutlineInputBorder(),
+                  floatingLabelBehavior: FloatingLabelBehavior.always)),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          child: TextFormField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                  labelText: "Select a password",
+                  border: OutlineInputBorder(),
+                  floatingLabelBehavior: FloatingLabelBehavior.always)),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          child: TextFormField(
+              controller: _repasswordController,
+              decoration: const InputDecoration(
+                  labelText: "Re-enter password",
+                  border: OutlineInputBorder(),
+                  floatingLabelBehavior: FloatingLabelBehavior.always)),
+        ),
+        const SizedBox(height: 24),
+        ElevatedButton(
+            onPressed: () {
+              print(_usernameController.text);
+              print(_passwordController.text);
+            },
+            child: const Text("Register"))
+      ]),
     );
   }
 }
