21f0efc1e6eb2363a2334f90115bd61ce6c567fd
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-02-11 07:38:43 +0530

    [Client] Change name register-page-1 to regiter-page-2. Change stateless widget to stateful widget in register-page-2

--- client/lib/pages/register_screen.dart
+++ client/lib/pages/register_screen.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:date_field/date_field.dart';
 
-class RegisterPage extends StatelessWidget {
-  const RegisterPage({super.key});
+class RegisterPage2 extends StatefulWidget {
+  const RegisterPage2({super.key});
+
+  @override
+  State<RegisterPage2> createState() => _RegisterPage2State();
+}
+
+class _RegisterPage2State extends State<RegisterPage2> {
+  final TextEditingController _dateController = TextEditingController();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime(DateTime.now().year - 16, 1, 1),
+      firstDate: DateTime(DateTime.now().year - 95, 1, 1),
+      lastDate: DateTime(DateTime.now().year - 16, 12, 31),
+    );
+
+    if (picked != null) {
+      setState(() {
+        _dateController.text = picked.toLocal().toString().split(' ')[0];
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,59 +40,62 @@ class RegisterPage extends StatelessWidget {
             )
           ]),
         ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              child: TextFormField(
+        body: Column(children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                  labelText: "First Name",
+                  border: OutlineInputBorder(),
+                  floatingLabelBehavior: FloatingLabelBehavior.always),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                  labelText: "Last Name",
+                  border: OutlineInputBorder(),
+                  floatingLabelBehavior: FloatingLabelBehavior.always),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                  labelText: "NIC Number",
+                  border: OutlineInputBorder(),
+                  floatingLabelBehavior: FloatingLabelBehavior.always),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: DropdownButtonFormField(
                 decoration: const InputDecoration(
-                    labelText: "First Name",
-                    border: OutlineInputBorder(),
-                    floatingLabelBehavior: FloatingLabelBehavior.always),
-              ),
+                  labelText: "Gender",
+                  border: OutlineInputBorder(),
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                ),
+                items: ['Male', 'Female', 'Other']
+                    .map((gender) =>
+                        DropdownMenuItem(value: gender, child: Text(gender)))
+                    .toList(),
+                onChanged: (String? selectedGender) =>
+                    {print('Gender: $selectedGender')}),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                  labelText: "Date of Birth",
+                  border: OutlineInputBorder(),
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  suffixIcon: Icon(Icons.calendar_today)),
+              controller: _dateController,
+              readOnly: true,
+              onTap: () => _selectDate(context),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                    labelText: "Last Name",
-                    border: OutlineInputBorder(),
-                    floatingLabelBehavior: FloatingLabelBehavior.always),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                    labelText: "NIC Number",
-                    border: OutlineInputBorder(),
-                    floatingLabelBehavior: FloatingLabelBehavior.always),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              child: DropdownButtonFormField(
-                  decoration: const InputDecoration(
-                      labelText: "Gender", border: OutlineInputBorder()),
-                  items: ['Male', 'Female', 'Other']
-                      .map((gender) =>
-                          DropdownMenuItem(value: gender, child: Text(gender)))
-                      .toList(),
-                  onChanged: (String? selectedGender) =>
-                      {print('Gender: $selectedGender')}),
-            ),
-            Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                child: DateTimeField(
-                  decoration: const InputDecoration(
-                      labelText: "Birth Date", border: OutlineInputBorder()),
-                  mode: DateTimeFieldPickerMode.date,
-                  firstDate: DateTime(1924),
-                  lastDate: DateTime(2009),
-                  onChanged: (DateTime? value) => print(value),
-                ))
-          ],
-        ));
+          )
+        ]));
   }
 }
