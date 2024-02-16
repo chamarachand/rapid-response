13995d94e8bff9d825fc26abc909c73b40000bdf
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-02-16 14:26:45 +0530

    [Client] Add validations for the first name, last name, nic, gender and birthdate fields in the register screen 2

--- client/lib/pages/register_screen_2.dart
+++ client/lib/pages/register_screen_2.dart
@@ -1,5 +1,6 @@
 import 'package:client/providers/registration_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'register_screen_3.dart';
 
@@ -11,6 +12,7 @@ class RegisterPage2 extends StatefulWidget {
 }
 
 class _RegisterPage2State extends State<RegisterPage2> {
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController _fnameController = TextEditingController();
   final TextEditingController _lnameController = TextEditingController();
   final TextEditingController _nicnoController = TextEditingController();
@@ -49,81 +51,116 @@ class _RegisterPage2State extends State<RegisterPage2> {
             )
           ]),
         ),
-        body: Column(children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            child: TextFormField(
-              controller: _fnameController,
-              decoration: const InputDecoration(
-                  labelText: "First Name",
-                  border: OutlineInputBorder(),
-                  floatingLabelBehavior: FloatingLabelBehavior.always),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            child: TextFormField(
-              controller: _lnameController,
-              decoration: const InputDecoration(
-                  labelText: "Last Name",
-                  border: OutlineInputBorder(),
-                  floatingLabelBehavior: FloatingLabelBehavior.always),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            child: TextFormField(
-              controller: _nicnoController,
-              decoration: const InputDecoration(
-                  labelText: "NIC Number",
-                  border: OutlineInputBorder(),
-                  floatingLabelBehavior: FloatingLabelBehavior.always),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            child: DropdownButtonFormField(
+        body: Form(
+          key: _formKey,
+          child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: TextFormField(
+                controller: _fnameController,
                 decoration: const InputDecoration(
-                  labelText: "Gender",
-                  border: OutlineInputBorder(),
-                  floatingLabelBehavior: FloatingLabelBehavior.always,
-                ),
-                items: ['Male', 'Female', 'Other']
-                    .map((gender) =>
-                        DropdownMenuItem(value: gender, child: Text(gender)))
-                    .toList(),
-                onChanged: (String? selectedGender) =>
-                    {_genderController.text = selectedGender ?? ""}),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            child: TextFormField(
-              controller: _birthdateController,
-              decoration: const InputDecoration(
-                  labelText: "Date of Birth",
-                  border: OutlineInputBorder(),
-                  floatingLabelBehavior: FloatingLabelBehavior.always,
-                  suffixIcon: Icon(Icons.calendar_month)),
-              readOnly: true,
-              onTap: () => _selectDate(context),
+                    labelText: "First Name",
+                    border: OutlineInputBorder(),
+                    floatingLabelBehavior: FloatingLabelBehavior.always),
+                validator: (value) {
+                  value = value!
+                      .trim(); // In a TextFormField f the user doesn't enter anything, the value returned will be an empty string "", not null.
+                  if (value.isEmpty) return "This field is required";
+                  if (value.length < 2) return "First name too short";
+                  if (value.length > 50) return "First name too long";
+                  return null;
+                },
+              ),
             ),
-          ),
-          ElevatedButton(
-              onPressed: () {
-                civilianPrvider.updateUser(
-                    firstName: _fnameController.text,
-                    lastName: _lnameController.text,
-                    nicNumber: _nicnoController.text,
-                    gender: _genderController.text,
-                    dateOfBirth: _birthdateController.text != ""
-                        ? DateTime.parse(_birthdateController.text)
-                        : null); // check whether there is another way (ex: directly take as DateTime)
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const RegisterPage3()));
-              },
-              child: const Text("Continue"))
-        ]));
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: TextFormField(
+                controller: _lnameController,
+                decoration: const InputDecoration(
+                    labelText: "Last Name",
+                    border: OutlineInputBorder(),
+                    floatingLabelBehavior: FloatingLabelBehavior.always),
+                validator: (value) {
+                  value = value!.trim();
+                  if (value.isEmpty) return "This field is required";
+                  if (value.length < 2) return "Last name too short";
+                  if (value.length > 50) return "Last name too long";
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: TextFormField(
+                controller: _nicnoController,
+                decoration: const InputDecoration(
+                    labelText: "NIC Number",
+                    border: OutlineInputBorder(),
+                    floatingLabelBehavior: FloatingLabelBehavior.always),
+                inputFormatters: [
+                  FilteringTextInputFormatter.deny(
+                      RegExp(r'\s')) // Prevent entering spaces
+                ],
+                validator: (value) {
+                  value = value!.trim();
+                  if (value.isEmpty) return "This field is required";
+                  if (value.length < 9) {
+                    return "Invalid NIC number";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                child: DropdownButtonFormField(
+                  decoration: const InputDecoration(
+                    labelText: "Gender",
+                    border: OutlineInputBorder(),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                  ),
+                  items: ['Male', 'Female', 'Other']
+                      .map((gender) =>
+                          DropdownMenuItem(value: gender, child: Text(gender)))
+                      .toList(),
+                  onChanged: (String? selectedGender) =>
+                      {_genderController.text = selectedGender ?? ""},
+                  validator: (value) =>
+                      (value == null) ? "This field is required" : null,
+                )),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: TextFormField(
+                  controller: _birthdateController,
+                  decoration: const InputDecoration(
+                      labelText: "Date of Birth",
+                      border: OutlineInputBorder(),
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      suffixIcon: Icon(Icons.calendar_month)),
+                  readOnly: true,
+                  onTap: () => _selectDate(context),
+                  validator: (value) =>
+                      (value == "") ? "This field is required" : null),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  if (!_formKey.currentState!.validate()) return;
+
+                  civilianPrvider.updateUser(
+                      firstName: _fnameController.text,
+                      lastName: _lnameController.text,
+                      nicNumber: _nicnoController.text,
+                      gender: _genderController.text,
+                      dateOfBirth: DateTime.parse(_birthdateController
+                          .text)); // check whether there is another way (ex: directly take as DateTime)
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const RegisterPage3()));
+                },
+                child: const Text("Continue"))
+          ]),
+        ));
   }
 }
