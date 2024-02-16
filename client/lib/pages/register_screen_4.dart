c89dad30e679f99514f214c299bca9f3ca4ff6c6
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-02-16 15:18:49 +0530

    [Client] Add input validations for the username field in the register screen 4

--- client/lib/pages/register_screen_4.dart
+++ client/lib/pages/register_screen_4.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:provider/provider.dart';
@@ -12,6 +13,7 @@ class RegisterPage4 extends StatefulWidget {
 }
 
 class _RegisterScreen4State extends State<RegisterPage4> {
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _repasswordController = TextEditingController();
@@ -106,48 +108,67 @@ class _RegisterScreen4State extends State<RegisterPage4> {
           )
         ]),
       ),
-      body: Column(children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          child: TextFormField(
+      body: Form(
+        key: _formKey,
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: TextFormField(
               controller: _usernameController,
               decoration: const InputDecoration(
                   labelText: "Select your username",
                   border: OutlineInputBorder(),
-                  floatingLabelBehavior: FloatingLabelBehavior.always)),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          child: TextFormField(
-              controller: _passwordController,
-              obscureText: true, //hide text
-              decoration: const InputDecoration(
-                  labelText: "Select a password",
-                  border: OutlineInputBorder(),
-                  floatingLabelBehavior: FloatingLabelBehavior.always)),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          child: TextFormField(
-              controller: _repasswordController,
-              obscureText: true, //hide text
-              decoration: const InputDecoration(
-                  labelText: "Re-enter password",
-                  border: OutlineInputBorder(),
-                  floatingLabelBehavior: FloatingLabelBehavior.always)),
-        ),
-        const SizedBox(height: 24),
-        ElevatedButton(
-            onPressed: () async {
-              civilianProvider.updateUser(
-                username: _usernameController.text,
-                password: _passwordController.text,
-              );
-              await registerCivilian(
-                  civilianProvider); // check whether 'await' is necessary
-            },
-            child: const Text("Register"))
-      ]),
+                  floatingLabelBehavior: FloatingLabelBehavior.always),
+              inputFormatters: [
+                FilteringTextInputFormatter.deny(RegExp(r'\s'))
+              ],
+              validator: (value) {
+                if (value!.isEmpty) return "This field is required";
+                if (value.startsWith(RegExp(r'\d'))) {
+                  return "Username cannot start with a number";
+                }
+                if (value.length < 4) {
+                  return "Username cannot be less than 4 characters";
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: TextFormField(
+                controller: _passwordController,
+                obscureText: true, //hide text
+                decoration: const InputDecoration(
+                    labelText: "Select a password",
+                    border: OutlineInputBorder(),
+                    floatingLabelBehavior: FloatingLabelBehavior.always)),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: TextFormField(
+                controller: _repasswordController,
+                obscureText: true, //hide text
+                decoration: const InputDecoration(
+                    labelText: "Re-enter password",
+                    border: OutlineInputBorder(),
+                    floatingLabelBehavior: FloatingLabelBehavior.always)),
+          ),
+          const SizedBox(height: 24),
+          ElevatedButton(
+              onPressed: () async {
+                if (!_formKey.currentState!.validate()) return;
+
+                civilianProvider.updateUser(
+                  username: _usernameController.text,
+                  password: _passwordController.text,
+                );
+                await registerCivilian(
+                    civilianProvider); // check whether 'await' is necessary
+              },
+              child: const Text("Register"))
+        ]),
+      ),
     );
   }
 }
