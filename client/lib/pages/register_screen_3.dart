694a691c2f5e8fb4d2c60ba8c2a3ad36d52f4eeb
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-02-16 10:16:44 +0530

    [Client] Add validations for mobile no input field in register screen 3

--- client/lib/pages/register_screen_3.dart
+++ client/lib/pages/register_screen_3.dart
@@ -11,6 +11,7 @@ class RegisterPage3 extends StatefulWidget {
 }
 
 class _RegisterPage3State extends State<RegisterPage3> {
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController _phonenoController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
 
@@ -19,55 +20,68 @@ class _RegisterPage3State extends State<RegisterPage3> {
     final civilianProvider = Provider.of<RegistrationProvider>(context);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Row(children: [
-          Text("Register"),
-          Padding(
-            padding: EdgeInsets.only(left: 8),
-            child: Icon(
-              Icons.person,
-              size: 32,
+        appBar: AppBar(
+          title: const Row(children: [
+            Text("Register"),
+            Padding(
+              padding: EdgeInsets.only(left: 8),
+              child: Icon(
+                Icons.person,
+                size: 32,
+              ),
+            )
+          ]),
+        ),
+        body: Form(
+          key: _formKey,
+          child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: TextFormField(
+                controller: _phonenoController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                    labelText: "Mobile No",
+                    prefixIcon: Icon(Icons.phone),
+                    border: OutlineInputBorder(),
+                    floatingLabelBehavior: FloatingLabelBehavior.always),
+                validator: (value) {
+                  if (value!.isEmpty) return "Please enter your phone number";
+                  if (value.length != 10) {
+                    return "Please enter a valid phone number";
+                  }
+                  return null;
+                },
+              ),
             ),
-          )
-        ]),
-      ),
-      body: Column(children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          child: TextFormField(
-            controller: _phonenoController,
-            decoration: const InputDecoration(
-                labelText: "Mobile No",
-                prefixIcon: Icon(Icons.phone),
-                border: OutlineInputBorder(),
-                floatingLabelBehavior: FloatingLabelBehavior.always),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          child: TextFormField(
-            controller: _emailController,
-            decoration: const InputDecoration(
-                labelText: "Email",
-                prefixIcon: Icon(Icons.email),
-                border: OutlineInputBorder(),
-                floatingLabelBehavior: FloatingLabelBehavior.always),
-          ),
-        ),
-        const SizedBox(height: 24),
-        ElevatedButton(
-            onPressed: () {
-              civilianProvider.updateUser(
-                mobileNumber: _phonenoController.text,
-                email: _emailController.text,
-              );
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const RegisterPage4()));
-            },
-            child: const Text("Continue"))
-      ]),
-    );
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: TextFormField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                    labelText: "Email",
+                    prefixIcon: Icon(Icons.email),
+                    border: OutlineInputBorder(),
+                    floatingLabelBehavior: FloatingLabelBehavior.always),
+              ),
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+                onPressed: () {
+                  if (!_formKey.currentState!.validate()) return;
+
+                  civilianProvider.updateUser(
+                    mobileNumber: _phonenoController.text,
+                    email: _emailController.text,
+                  );
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const RegisterPage4()));
+                },
+                child: const Text("Continue"))
+          ]),
+        ));
   }
 }
