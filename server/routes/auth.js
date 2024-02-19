38b64989de1648ad9acaae78e2b3307cc3a8972d
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-02-19 11:08:49 +0530

    [Server] get dependency to generate jwt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Joi = require("joi");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { Civilian } = require("../models/civilian");
 const { FirstResponder } = require("../models/first-responder");
 
