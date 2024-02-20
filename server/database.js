07b1c85638676cc3865a170a9dcf9905d3be9930
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-02-20 11:44:26 +0530

    [Server] Change cloud database name to rapid-response

--- server/database.js
+++ server/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 function connection() {
   const connectionUrlLocal = "mongodb://localhost:27017/rapid-response";
   const connectionUrlCloud =
-    "mongodb+srv://cham:Lasimdatlas2024@cluster0.iy1cxvg.mongodb.net/?retryWrites=true&w=majority";
+    "mongodb+srv://cham:Lasimdatlas2024@cluster0.iy1cxvg.mongodb.net/rapid-response?retryWrites=true&w=majority";
 
   mongoose
     .connect(connectionUrlCloud)
