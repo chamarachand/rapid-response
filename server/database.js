ef26372cafb2994ae696c136162d8ebde59aefd4
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-02-20 11:26:00 +0530

    [Server] Connect to mongodb cloud platform

--- server/database.js
+++ server/database.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
 function connection() {
-  const connectionUrl = "mongodb://localhost:27017/rapid-response";
+  const connectionUrlLocal = "mongodb://localhost:27017/rapid-response";
+  const connectionUrlCloud =
+    "mongodb+srv://cham:Lasimdatlas2024@cluster0.iy1cxvg.mongodb.net/?retryWrites=true&w=majority";
 
   mongoose
-    .connect(connectionUrl)
-    .then(() => console.log("Connected to MongoDB.."))
+    .connect(connectionUrlCloud)
+    .then(() => console.log("Connected to MongoDB Cloud.."))
     .catch((error) => console.error("Could not connect to MongoDB", error));
 }
 
