90efec1ccf41db679d34263131dd7913e7987f8d
Rank 3 <jayathu.shehan.2002@gmail.com>
2024-02-22 04:46:31 +0530

    Dev SOS page

--- client/lib/pages/SOS_page.dart
+++ client/lib/pages/SOS_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class SOSpage extends StatefulWidget {
@@ -13,6 +15,7 @@ class _SOSpageState extends State<SOSpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: const Text(
           'SOS',
@@ -31,46 +34,104 @@ class _SOSpageState extends State<SOSpage> {
             children: <Widget>[
               const CircleAvatar(
                 radius: 55,
-                backgroundColor: Color.fromARGB(255, 235, 235, 235),
+                backgroundColor: Color.fromARGB(227, 255, 78, 78),
                 child: Icon(
                   Icons.camera_alt,
                   size: 60,
-                  color: Colors.grey,
+                  color: Color.fromARGB(255, 43, 43, 43),
                 ),
               ),
               const SizedBox(height: 30),
               const CircleAvatar(
                 radius: 55,
-                backgroundColor: Color.fromARGB(255, 235, 235, 235),
-                child: Icon(Icons.mic, size: 60, color: Colors.grey),
+                backgroundColor: Color.fromARGB(227, 255, 78, 78),
+                child: Icon(Icons.mic,
+                    size: 60, color: Color.fromARGB(255, 43, 43, 43)),
               ),
               const SizedBox(
                 height: 30,
               ),
-              DropdownButton<String>(
-                borderRadius: const BorderRadius.all(Radius.circular(10)),
-                value: _chosenModel,
-                items: <String>['Accident', 'Fludding', 'Land Slide']
-                    .map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    _chosenModel = newValue!;
-                  });
-                },
-                hint: const Text(
-                  "Emergency Type",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 30,
-                    fontWeight: FontWeight.w600,
+              FractionallySizedBox(
+                alignment: Alignment.center,
+                widthFactor: 0.9,
+                child: Container(
+                  height: 50,
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(227, 255, 78, 78),
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  child: DropdownButton<String>(
+                    isExpanded: true,
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    value: _chosenModel,
+                    items: <String>[
+                      'Accident',
+                      'Fludding',
+                      'Land Slide',
+                      'Flud1ding',
+                      'Fludd2ing',
+                      'Fluddi3ng',
+                      'Fluddin4g',
+                      'Fludding5',
+                      '1Fludding',
+                      'F2ludding',
+                      'Fl3udding',
+                      'Flu4dding',
+                      'Flud5ding',
+                      'Fludd6ing',
+                      'Fluddi7ng',
+                      'Fluddin7g',
+                      'Fluddin8g'
+                    ].map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _chosenModel = newValue!;
+                      });
+                    },
+                    hint: const Text(
+                      "Emergency Type",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    dropdownColor: const Color.fromARGB(227, 255, 78, 78),
                   ),
                 ),
-                dropdownColor: const Color.fromARGB(228, 200, 200, 255),
+              ),
+              const SizedBox(height: 30),
+              FractionallySizedBox(
+                widthFactor: 0.9,
+                child: Container(
+                  height: 50,
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(227, 255, 78, 78),
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(3),
+                    child: TextField(
+                      decoration: InputDecoration(hintText: "Not in the list"),
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  print("Hello World");
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(227, 255, 78, 78),
+                ),
+                child: const Text('Send SOS'),
               )
             ],
           ),
