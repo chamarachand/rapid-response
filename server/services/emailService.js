e79a5afd2e66122ae384bc6ee82efd4164922267
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-02-25 10:22:32 +0530

    [Server] Update email text

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -6,8 +6,8 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true,
   auth: {
-    user: "chamaquarium@gmail.com",
-    pass: "mkxv fbfn tipy viqs",
+    user: "rapidresponse.srilanka@gmail.com",
+    pass: "omqk nfim hbbz srze",
   },
 });
 
@@ -16,32 +16,28 @@ async function sendRegisterConfirmationMail() {
     from: '"Rapid Response" <rapidresponse.srilanka@gmail.com>',
     to: "lasithachandrasekara@gmail.com",
     subject: " Welcome to Rapid Response - Your Emergency Assistance App!",
-    text: `Dear [User's Name],
+    html: `<p>Dear [User's Name],</p>
 
-    Welcome to Rapid Response, your reliable companion in times of emergency. We're thrilled to have you on board, and we want to extend a warm welcome to our community.
-    
-    Rapid Response is designed to provide you with swift assistance and support during critical situations. Whether you're a civilian seeking help or a first responder offering aid, our platform is here to connect you with the resources and assistance you need.
-    
-    As a registered user, you now have access to a range of features that empower you to respond effectively to emergencies:
-    
-    SOS Functionality: With just a few taps, you can send out an emergency distress signal, notifying nearby first responders and your designated emergency contacts.
-    
-    Incident Reporting: Easily report incidents and emergencies in your area, providing crucial information to responders and fellow users.
-    
-    Real-time Alerts: Stay informed about incidents and alerts in your vicinity, ensuring you're aware of any emergencies as they unfold.
-    
-    User Account Management: Keep your profile up-to-date with important information, ensuring that responders can reach you when needed most.
-    
-    Emergency Contact Linking: Connect with family members, friends, or trusted contacts to keep them informed and involved in your safety network.
-    
-    We encourage you to explore the app, familiarize yourself with its features, and reach out if you have any questions or feedback. Your safety and well-being are our top priorities, and we're committed to providing you with the support you need, when you need it.
-    
-    Thank you for joining us on this journey to make our communities safer and more resilient. Together, we can make a difference.
-    
-    Stay safe,
-    
-    [Your Name]
-    Rapid Response Team`,
+    <p>Welcome to <strong>Rapid Response</strong>, your reliable companion in times of emergency. We're thrilled to have you on board, and we want to extend a warm welcome to our community.</p>
+
+    <p><strong>Rapid Response</strong> is designed to provide you with swift assistance and support during critical situations. Whether you're a civilian seeking help or a first responder offering aid, our platform is here to connect you with the resources and assistance you need.</p>
+
+    <p>As a registered user, you now have access to a range of features that empower you to respond effectively to emergencies:</p>
+    <ul>
+        <li><strong>SOS Functionality:</strong> With just a few taps, you can send out an emergency distress signal, notifying nearby first responders and your designated emergency contacts.</li>
+        <li><strong>Incident Reporting:</strong> Easily report incidents and emergencies in your area, providing crucial information to responders and fellow users.</li>
+        <li><strong>Real-time Alerts:</strong> Stay informed about incidents and alerts in your vicinity, ensuring you're aware of any emergencies as they unfold.</li>
+        <li><strong>User Account Management:</strong> Keep your profile up-to-date with important information, ensuring that responders can reach you when needed most.</li>
+        <li><strong>Emergency Contact Linking:</strong> Connect with family members, friends, or trusted contacts to keep them informed and involved in your safety network.</li>
+    </ul>
+
+    <p>We encourage you to explore the app, familiarize yourself with its features, and reach out if you have any questions or feedback. Your safety and well-being are our top priorities, and we're committed to providing you with the support you need, when you need it.</p>
+
+    <p>Thank you for joining us on this journey to make our communities safer and more resilient. Together, we can make a difference.</p>
+
+    <p>Stay safe,</p>
+
+    <p>[Your Name]<br>Rapid Response Team</p>`,
   };
 
   try {
