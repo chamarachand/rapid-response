493d05023e949efb59f9b923e2c2bc2ea60340ec
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-02-27 15:39:57 +0530

    [Client] decode access token in dashboard

--- client/lib/pages/dashboard_test.dart
+++ client/lib/pages/dashboard_test.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:client/storage/user_secure_storage.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  void _loadToken() async {
+    final accessToken = await UserSecureStorage.getAccessToken();
+
+    if (accessToken != null) {
+      var decodedToken = JwtDecoder.decode(accessToken);
+      // Access token claims
+      print(decodedToken['username']);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadToken();
+  }
 
   @override
   Widget build(BuildContext context) {
--- client/lib/storage/user_secure_storage.dart
+++ client/lib/storage/user_secure_storage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class UserSecureStorage {
-  static final _storage = const FlutterSecureStorage();
+  static const _storage = FlutterSecureStorage();
 
   static void setAccessToken(String token) async {
     await _storage.write(key: "accessToken", value: token);
