1c2f8f7086b2c9ec17eb791c81bc1ef408c3e23b
Rank 3 <jayathu.shehan.2002@gmail.com>
2024-02-28 02:24:42 +0530

    Update

--- client/lib/pages/SOS_page.dart
+++ client/lib/pages/SOS_page.dart
@@ -1,8 +1,6 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:image_picker/image_picker.dart';
 
 class SOSpage extends StatefulWidget {
   const SOSpage({super.key});
