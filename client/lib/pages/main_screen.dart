34b4a572d12d1eca45b9f94db95b108bb8dc316a
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-02-28 20:45:15 +0530

    [Client] Add load token logic to main screen

--- client/lib/pages/main_screen.dart
+++ client/lib/pages/main_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'SOS_page.dart';
 import 'report_incident_screen.dart';
+import 'package:client/storage/user_secure_storage.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 
 class MainMenu extends StatefulWidget {
   const MainMenu({super.key});
@@ -10,61 +12,78 @@ class MainMenu extends StatefulWidget {
 
 class MainMenuScreen extends State<MainMenu> {
   int _selectedIndex = 1;
-  
+
+  var _firstName = "";
+
+  void _loadToken() async {
+    final accessToken = await UserSecureStorage.getAccessToken();
+
+    if (accessToken != null) {
+      var decodedToken = JwtDecoder.decode(accessToken);
+      // Access token claims
+      setState(() {
+        _firstName = decodedToken["firstName"];
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadToken();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-       title: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Builder(
-          builder: (BuildContext context) {
-            double appBarHeight = AppBar().preferredSize.height;
-            return Container(
-              alignment: Alignment.center,
-              child: IconButton(
-                icon: SizedBox(
-                  width: appBarHeight-20,
-                  height: appBarHeight-20,
-                  child: Image.asset('assets/RR_logo.png'),
-                ),
-                onPressed: () {
-                },
-              ),
-            );
-          },
-        ),
-        Builder(
-          builder: (BuildContext context) {
-            double appBarHeight = AppBar().preferredSize.height;
-            return Container(
-              alignment: Alignment.center,
-              child: PopupMenuButton(
-                icon: SizedBox(
-                  width: appBarHeight,
-                  height: appBarHeight,
-                  child: const Icon(Icons.person),
-                ),
-                itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Builder(
+              builder: (BuildContext context) {
+                double appBarHeight = AppBar().preferredSize.height;
+                return Container(
+                  alignment: Alignment.center,
+                  child: IconButton(
+                    icon: SizedBox(
+                      width: appBarHeight - 20,
+                      height: appBarHeight - 20,
+                      child: Image.asset('assets/RR_logo.png'),
+                    ),
+                    onPressed: () {},
+                  ),
+                );
+              },
+            ),
+            Builder(
+              builder: (BuildContext context) {
+                double appBarHeight = AppBar().preferredSize.height;
+                return Container(
+                  alignment: Alignment.center,
+                  child: PopupMenuButton(
+                    icon: SizedBox(
+                      width: appBarHeight,
+                      height: appBarHeight,
+                      child: const Icon(Icons.person),
+                    ),
+                    itemBuilder: (BuildContext context) => <PopupMenuEntry>[
                       PopupMenuItem(
                         child: ListTile(
-                          title: const Center(
-                            child: Text('Logout')
-                            ),
+                          title: const Center(child: Text('Logout')),
                           onTap: () {
                             Navigator.pop(context);
                           },
                         ),
                       ),
                     ],
-              ),
-            );
-          },
-        ), 
-        ],
-       ),
-      backgroundColor: const Color(0xFF8497B0), 
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+        backgroundColor: const Color(0xFF8497B0),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -75,38 +94,42 @@ class MainMenuScreen extends State<MainMenu> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               ElevatedButton(
-                onPressed: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const SOSpage())),
+                onPressed: () => Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const SOSpage())),
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red,
-                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 30),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  )
-                ),
+                    backgroundColor: Colors.red,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 50, vertical: 30),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    )),
                 child: const Text(
                   'SOS',
-                  style: TextStyle(fontSize: 30, color: Colors.white,),
-                ),  
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                  ),
+                ),
               ),
               ElevatedButton(
                 onPressed: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const ReportScreen())),
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ReportScreen())),
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red,
-                  padding: const EdgeInsets.symmetric(horizontal: 33, vertical: 30),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  )
-                ),
+                    backgroundColor: Colors.red,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 33, vertical: 30),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    )),
                 child: const Text(
                   'Report',
-                  style: TextStyle(fontSize: 30, color: Colors.white,),
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
                   ),
+                ),
               ),
             ],
           ),
@@ -116,16 +139,19 @@ class MainMenuScreen extends State<MainMenu> {
               // Implement Map functionality
             },
             style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFFC06565),
-                  padding: const EdgeInsets.symmetric(horizontal: 138, vertical: 20),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  )
-                ),
+                backgroundColor: const Color(0xFFC06565),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 138, vertical: 20),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                )),
             child: const Text(
               'MAP',
-              style: TextStyle(fontSize: 30, color: Colors.white,),
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
               ),
+            ),
           ),
           const SizedBox(height: 25),
           ElevatedButton(
@@ -133,69 +159,73 @@ class MainMenuScreen extends State<MainMenu> {
               // Implement Regitered Locations functionality
             },
             style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFFC06565),
-                  padding: const EdgeInsets.symmetric(horizontal: 29, vertical: 20),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  )
-                ),
+                backgroundColor: const Color(0xFFC06565),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 29, vertical: 20),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                )),
             child: const Text(
               'Registered Locations',
-              style: TextStyle(fontSize: 30, color: Colors.white,),
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
               ),
+            ),
           ),
           const SizedBox(height: 25),
           Expanded(
-              child: Container(
-                color: Colors.grey[300],
-                child: ListView.builder(
-                  itemCount: 10, // Example notification count
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text('Notification $index'),
-                      // Add more details here if needed
-                    );
-                  },
-                ),
+            child: Container(
+              color: Colors.grey[300],
+              child: ListView.builder(
+                itemCount: 10, // Example notification count
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('Notification $index'),
+                    // Add more details here if needed
+                  );
+                },
               ),
             ),
-          ],
+          ),
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: const Color(0xFFD9D9D9),
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        items: [
-          BottomNavigationBarItem(
-            icon: IconButton(
-              icon: const Icon(Icons.link),
-              onPressed: () {
-                _onItemTapped(0);
-              },
+          backgroundColor: const Color(0xFFD9D9D9),
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: [
+            BottomNavigationBarItem(
+              icon: IconButton(
+                icon: const Icon(Icons.link),
+                onPressed: () {
+                  _onItemTapped(0);
+                },
+              ),
+              label: 'Link',
             ),
-            label: 'Link',
-          ),
-          BottomNavigationBarItem(
-            icon: IconButton(
-              icon: const Icon(Icons.home),
-              onPressed: () {
-                _onItemTapped(1);
-              },
+            BottomNavigationBarItem(
+              icon: IconButton(
+                icon: const Icon(Icons.home),
+                onPressed: () {
+                  _onItemTapped(1);
+                },
+              ),
+              label: 'Home',
             ),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: IconButton(
-              icon: const Icon(Icons.history),
-              onPressed: () {
-                _onItemTapped(2);
-              },
+            BottomNavigationBarItem(
+              icon: IconButton(
+                icon: const Icon(Icons.history),
+                onPressed: () {
+                  _onItemTapped(2);
+                },
+              ),
+              label: 'History',
             ),
-            label: 'History',
-          ),
-      ]),
+          ]),
     );
   }
+
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
--- client/lib/pages/welcome_screen.dart
+++ client/lib/pages/welcome_screen.dart
@@ -5,6 +5,7 @@ import 'login_screen.dart';
 import 'register_screen_2.dart';
 import 'SOS_page.dart';
 import 'add_em_comtact_screen.dart';
+import 'main_screen.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
