b2f0301b5c2411bf821dfbd3481a47adf4e31b1b
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-03-01 12:53:34 +0530

    [Client] Configure firebase for android

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,10 +1,18 @@
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:client/authenticator.dart';
 import 'package:provider/provider.dart';
 import 'providers/registration_provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: 'AIzaSyB3-XWUh108BvTcdwiuC1jJgTtJGB0H1zQ',
+          appId: '1:198757308599:android:65fe518f39f3c266769532',
+          messagingSenderId: '198757308599',
+          projectId: 'rapid-response-802d3'));
+
   runApp(const MyApp());
 }
 
