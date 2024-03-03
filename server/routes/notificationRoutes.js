14ac738274c23bb13f104d29d4896d1cb732d81f
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-03-03 22:02:23 +0530

    [Server] Implement notification saving

--- server/routes/notificationRoutes.js
+++ server/routes/notificationRoutes.js
@@ -3,14 +3,35 @@ const router = express.Router();
 var admin = require("firebase-admin");
 const serviceAccount = require("../rapid-response-802d3-firebase-adminsdk-n69t0-43af2556f7.json");
 const { Civilian } = require("../models/civilian");
+const { Notification, validate } = require("../models/notification");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 router.post("/", async (req, res) => {
-  const { to, title, body } = req.body; // Later validate this
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
+  const { from, to, title, body } = req.body; // Later validate this
+
+  // Save to db
+  try {
+    const notification = new Notification({
+      from: from,
+      to: to,
+      title: title,
+      body: body,
+      timestamp: new Date(),
+    });
+
+    await notification.save();
+  } catch (error) {
+    console.log("Error: ");
+    res.status(500).send(error);
+  }
+
+  // Send notification
   const { fcmToken } = await Civilian.findById(to).select("fcmToken"); // Add if not token logic
   if (!fcmToken) return res.status(404).send("Reciever FCM token not found");
 
@@ -26,6 +47,7 @@ router.post("/", async (req, res) => {
     res.send("Send Success");
   } catch (error) {
     console.error("Error: " + error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
