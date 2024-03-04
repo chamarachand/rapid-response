093aa2c75c59729127e1196546ed5fb58d8bd2c2
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-03-04 06:45:05 +0530

    [Server] update the fcmToken maxlength to 255 in user sharedSchema

--- server/common/sharedSchema.js
+++ server/common/sharedSchema.js
@@ -26,7 +26,7 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
   password: { type: String, minlength: 8, maxlength: 255, required: true },
-  fcmToken: { type: String, default: null },
+  fcmToken: { type: String, maxlength: 255, default: null },
 });
 
 // Convert Date into UTC format before saving in the database
