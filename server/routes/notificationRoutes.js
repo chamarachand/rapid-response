0fbe70b02fa08ec9e24e0b0cbe9d8e3923c77a52
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-03-04 16:59:57 +0530

    [Server] Create a get route handler in notification for checking whether if the request is already been send befor for being added as emergency contact

--- server/routes/notificationRoutes.js
+++ server/routes/notificationRoutes.js
@@ -10,6 +10,7 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
+// Check if the inteded user is already an emergency contact
 router.get(
   "/search/emcontact/:currentUserId/:intendedUserId",
   async (req, res) => {
@@ -31,6 +32,34 @@ router.get(
   }
 );
 
+router.get(
+  "/search/request/:currentUserId/:intendedUserId",
+  async (req, res) => {
+    const { currentUserId, intendedUserId } = req.params;
+
+    if (!currentUserId || !intendedUserId)
+      return res.status(400).send("Missing parameter/s");
+
+    const intendedUser = await Civilian.findById(intendedUserId).select(
+      "notifications"
+    );
+
+    for (let objectId of intendedUser.notifications) {
+      const notification = await Notification.findById(objectId);
+
+      if (
+        notification &&
+        notification.from == currentUserId && // replace == with === when connected with front end
+        notification.type === "emergency-contact-request" &&
+        !notification.responded
+      )
+        return res.status(200).send("Request for intended user already sent");
+    }
+
+    return res.status(404).send("Request for intended user not sent");
+  }
+);
+
 router.post("/send", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
@@ -43,7 +72,7 @@ router.post("/send", async (req, res) => {
     notification = new Notification({
       from: from,
       to: to,
-      type: "emeregency-contact-request",
+      type: "emergency-contact-request",
       title: title,
       body: body,
       timestamp: new Date().toLocaleString(),
