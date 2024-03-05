68bad0dfece9af36af4a5186d05024835e34ddb2
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-03-05 06:51:48 +0530

    [Client] Update import statements in welcome screen and main screen due to the previous refactor

--- client/lib/pages/main_screen.dart
+++ client/lib/pages/main_screen.dart
@@ -4,7 +4,7 @@ import 'report_incident_screen.dart';
 import 'package:client/pages/welcome_screen.dart';
 import 'package:client/storage/user_secure_storage.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
-import 'package:client/pages/add_em_comtact_screen.dart';
+import 'package:client/pages/link_accounts/add_em_comtact_screen.dart';
 
 class MainMenu extends StatefulWidget {
   const MainMenu({super.key});
