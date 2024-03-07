81558625b3b7827dfe9748ac3b5a759ba0dcf5c2
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-03-07 16:57:01 +0530

    [Client] Wrap register screen 3 with SingleChildScroll View

--- client/lib/pages/register_screen_3.dart
+++ client/lib/pages/register_screen_3.dart
@@ -40,81 +40,87 @@ class _RegisterPage3State extends State<RegisterPage3> {
         body: Center(
           child: Form(
             key: _formKey,
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Image.asset("assets/contact-info.png"),
-              const SizedBox(height: 10),
-              const LabelTextRegister("Phone Number"),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(20, 0, 20, 15),
-                child: TextFormField(
-                  controller: _phonenoController,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    prefixIcon: const Icon(Icons.phone),
-                    floatingLabelBehavior: FloatingLabelBehavior.always,
-                    fillColor: const Color.fromARGB(255, 241, 228, 228),
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
+            child: SingleChildScrollView(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset("assets/contact-info.png"),
+                    const SizedBox(height: 10),
+                    const LabelTextRegister("Phone Number"),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 15),
+                      child: TextFormField(
+                        controller: _phonenoController,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(Icons.phone),
+                          floatingLabelBehavior: FloatingLabelBehavior.always,
+                          fillColor: const Color.fromARGB(255, 241, 228, 228),
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 18),
+                        ),
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        validator: (value) {
+                          if (value!.isEmpty)
+                            return "Please enter your phone number";
+                          if (value.length != 10) {
+                            return "Please enter a valid phone number";
+                          }
+                          return null;
+                        },
+                      ),
                     ),
-                    contentPadding: const EdgeInsets.symmetric(
-                        horizontal: 10, vertical: 18),
-                  ),
-                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                  validator: (value) {
-                    if (value!.isEmpty) return "Please enter your phone number";
-                    if (value.length != 10) {
-                      return "Please enter a valid phone number";
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              const LabelTextRegister("Email"),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(20, 0, 20, 15),
-                child: TextFormField(
-                  controller: _emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    prefixIcon: const Icon(Icons.email),
-                    floatingLabelBehavior: FloatingLabelBehavior.always,
-                    fillColor: const Color.fromARGB(255, 241, 228, 228),
-                    filled: true,
-                    contentPadding: const EdgeInsets.symmetric(
-                        horizontal: 10, vertical: 18),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
+                    const LabelTextRegister("Email"),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 15),
+                      child: TextFormField(
+                        controller: _emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(Icons.email),
+                          floatingLabelBehavior: FloatingLabelBehavior.always,
+                          fillColor: const Color.fromARGB(255, 241, 228, 228),
+                          filled: true,
+                          contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 18),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        inputFormatters: [
+                          FilteringTextInputFormatter.deny(
+                              RegExp(r'\s')) // Prevent entering spaces
+                        ],
+                        validator: (value) {
+                          return EmailValidator.validate(value!)
+                              ? null
+                              : "Please enter a valid email address";
+                        },
+                      ),
                     ),
-                  ),
-                  inputFormatters: [
-                    FilteringTextInputFormatter.deny(
-                        RegExp(r'\s')) // Prevent entering spaces
-                  ],
-                  validator: (value) {
-                    return EmailValidator.validate(value!)
-                        ? null
-                        : "Please enter a valid email address";
-                  },
-                ),
-              ),
-              const SizedBox(height: 10),
-              ElevatedButton(
-                  onPressed: () {
-                    if (!_formKey.currentState!.validate()) return;
+                    const SizedBox(height: 10),
+                    ElevatedButton(
+                        onPressed: () {
+                          if (!_formKey.currentState!.validate()) return;
 
-                    civilianProvider.updateUser(
-                      mobileNumber: _phonenoController.text,
-                      email: _emailController.text,
-                    );
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const RegisterPage4()));
-                  },
-                  child: const Text("Continue"))
-            ]),
+                          civilianProvider.updateUser(
+                            mobileNumber: _phonenoController.text,
+                            email: _emailController.text,
+                          );
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const RegisterPage4()));
+                        },
+                        child: const Text("Continue"))
+                  ]),
+            ),
           ),
         ));
   }
