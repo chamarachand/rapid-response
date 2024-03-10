60ef41b437fbaa696ed68c57efba09cfcaa5c251
H.S.C.Samarasinghe <chathurangasahan2002@gmail.com>
2024-03-10 14:53:54 +0530

    commit before push

--- client/lib/pages/profile_screen.dart
+++ client/lib/pages/profile_screen.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'SOS_page.dart';
-import 'report_incident_screen.dart';
-import 'package:client/pages/welcome_screen.dart';
 import 'package:client/storage/user_secure_storage.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:client/pages/link_accounts/add_em_comtact_screen.dart';
