feba6b6ebbccad7c60f9faa7c23448d9d5bb8b09
H.S.C.Samarasinghe <chathurangasahan2002@gmail.com>
2024-03-10 20:47:31 +0530

    added url_launcher to line 55 of pubspec.yaml and changed android sdk version to 34 in line 28 of client\android\app\build.gradle to facilitate url_launcher

---
+++ client/lib/pages/register_new_location.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:client/storage/user_secure_storage.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:client/pages/link_accounts/add_em_comtact_screen.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+class RegisterLocation extends StatefulWidget {
+  const RegisterLocation({super.key});
+  @override
+  RegisterNewLocation createState() => RegisterNewLocation();
+}
+
+class RegisterNewLocation extends State<RegisterLocation> {
+  String locationMessage = 'Current Location';
+  late String lat;
+  late String long;
+
+  int _selectedIndex = 1;
+
+  final profileImg =
+      "https://icons.iconarchive.com/icons/papirus-team/papirus-status/256/avatar-default-icon.png";
+
+  var _firstName = "";
+
+  void _loadToken() async {
+    final accessToken = await UserSecureStorage.getAccessToken();
+
+    if (accessToken != null) {
+      var decodedToken = JwtDecoder.decode(accessToken);
+      // Access token claims
+      setState(() {
+        _firstName = decodedToken["firstName"];
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadToken();
+  }
+
+  Future<Position> _getCurrentLocation() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location service disabled');
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permission denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error('Location permission is permanently denied. We cannot access yoour location.');
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+
+  void _liveLocation() {
+    LocationSettings locationSettings = const LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 100,
+    );
+
+    Geolocator.getPositionStream(locationSettings: locationSettings).listen((Position position) {
+      lat = position.latitude.toString();
+      long = position.longitude.toString();
+
+      setState(() {
+        locationMessage = 'Latitude: $lat, Longitude: $long';
+      });
+    });
+  }
+
+  Future<void> _openMap(String lat, String long) async {
+    String googleURL = 'https://www.google.com/maps/search/?api=1&query=$lat,$long';
+    await canLaunchUrlString(googleURL)
+      ? await launchUrlString(googleURL)
+      : throw 'Could not launch $googleURL';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Register New Locations',
+          style: TextStyle(
+            color: Color.fromRGBO(0, 0, 0, 1),
+            fontSize: 25,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+        backgroundColor: Color.fromARGB(255, 0, 88, 202),
+        ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(locationMessage, textAlign: TextAlign.center,),
+            const SizedBox(height: 20,),
+            ElevatedButton(
+              onPressed: (){
+                _getCurrentLocation().then((value) {
+                  lat = '${value.latitude}';
+                  long = '${value.longitude}';
+                  setState(() {
+                    locationMessage = 'Latitude: $lat, Longitude: $long';
+                  });
+                  _liveLocation();
+                });
+              }, 
+              child: Text('Get Current Location'),
+              ),
+            const SizedBox(height: 20,),
+            ElevatedButton(
+              onPressed: (){
+                _openMap(lat, long);
+              }, 
+              child: const Text('Open Google Map'),
+              ),
+          ],
+        ),
+      ),
+      
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: const Color(0xFFD9D9D9), 
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: [
+            BottomNavigationBarItem(
+              icon: IconButton(
+                icon: const Icon(Icons.link),
+                onPressed: () {
+                  _onItemTapped(0);
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: ((context) => const UserSearchPage())));
+                },
+              ),
+              label: 'Link',
+            ),
+            BottomNavigationBarItem(
+              icon: IconButton(
+                icon: const Icon(Icons.home),
+                onPressed: () {
+                  _onItemTapped(1);
+                },
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: IconButton(
+                icon: const Icon(Icons.history),
+                onPressed: () {
+                  _onItemTapped(2);
+                },
+              ),
+              label: 'History',
+            ),
+          ]),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
