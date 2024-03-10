ce9d00d5700f9d40edb1c8736eb8611c5c67567f
Navidu <navidubc@gmail.com>
2024-03-10 13:24:10 +0530

    update welcome screen

--- client/lib/pages/welcome_screen.dart
+++ client/lib/pages/welcome_screen.dart
@@ -1,11 +1,7 @@
-import 'package:client/pages/add_event.dart';
-import 'package:client/pages/report_incident_screen.dart';
 import 'package:flutter/material.dart';
 import 'login_screen.dart';
 import 'register_screen_2.dart';
-import 'SOS_page.dart';
-import 'link_accounts/add_em_comtact_screen.dart';
-import 'main_screen.dart';
+
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -14,61 +10,89 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Welcome Page"),
+          title:null,
         ),
-        body: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-                child: ElevatedButton(
-                    onPressed: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const LoginPage())),
-                    child: const Text("Login"))),
-            Center(
-                child: ElevatedButton(
-                    onPressed: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const MainMenu())),
-                    child: const Text("Main menu"))),
-            Center(
-                child: ElevatedButton(
-                    onPressed: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const RegisterPage2())),
-                    child: const Text("Register"))),
-            Center(
-                child: ElevatedButton(
-                    onPressed: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const SOSpage())),
-                    child: const Text("SOS Page (Test)"))),
-            Center(
-                child: ElevatedButton(
-                    onPressed: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const ReportScreen())),
-                    child: const Text("To Report Screen (Test)"))),
-            Center(
-                child: ElevatedButton(
-                    onPressed: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const add_event())),
-                    child: const Text("To Add Event (Test)"))),
-            Center(
-                child: ElevatedButton(
-                    onPressed: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const UserSearchPage())),
-                    child: const Text("Add Emergency Contacts")))
-          ],
-        ));
+        body: Container(
+          child: Container(
+            
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(colors:[
+                Color.fromRGBO(253, 179, 179, 1),
+                Color.fromRGBO(255, 189, 189, 1),
+                Color.fromRGBO(250, 213, 213, 1),
+                Color.fromRGBO(255, 230, 230, 1),
+                Color.fromRGBO(255, 255, 255, 1),
+              ],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+              ),
+          
+            ),
+            
+            child:Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              
+               children:[
+                Center(
+                  child:Image.asset("assets/doctor_2.png") ,
+                ),
+                const SizedBox(height: 95,),
+                
+                
+               Center(
+                
+                
+                 child: ElevatedButton(
+                          onPressed: () => Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => const LoginPage()) ),
+                              
+
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color.fromRGBO(51, 153, 255, 1),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                            fixedSize: Size(MediaQuery.of(context).size.width*0.85,
+                            MediaQuery.of(context).size.height*0.08
+                            ),
+                            ),
+                          child: const Text("Sign in",
+                          style: TextStyle(fontSize: 20),
+                          ),
+                          ),
+                          
+                          
+                  ),
+              const SizedBox(height: 15,),
+             Center(
+              
+                     child: ElevatedButton(
+                        onPressed: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const RegisterPage2())),
+                        style: ElevatedButton.styleFrom(
+                      
+                          backgroundColor: const Color.fromRGBO(51, 153, 255, 1),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          //padding: EdgeInsets.symmetric(horizontal: 120, vertical: 20)
+                          fixedSize: Size(MediaQuery.of(context).size.width*0.85,
+                          MediaQuery.of(context).size.height*0.08
+                          )
+                        ),
+                        child: const Text("Sign up",
+                      style: TextStyle(fontSize: 20),
+                        ),
+                        ),
+                                                     
+                   ),
+                   ]
+             
+            )
+            ),
+        )
+          );
   }
 }
