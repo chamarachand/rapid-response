1fc39fcf1b838a5218d79136058f89688cb4a8cb
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-03-10 18:42:09 +0530

    [Client] Rename 'signup' to 'Register'

--- client/lib/pages/welcome_screen.dart
+++ client/lib/pages/welcome_screen.dart
@@ -6,7 +6,6 @@ import 'link_accounts/add_em_comtact_screen.dart';
 import 'main_screen.dart';
 import 'register_screen_1.dart';
 
-
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
 
@@ -14,89 +13,80 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title:null,
+          title: null,
         ),
         body: Container(
           child: Container(
-            
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(colors:[
-                Color.fromRGBO(253, 179, 179, 1),
-                Color.fromRGBO(255, 189, 189, 1),
-                Color.fromRGBO(250, 213, 213, 1),
-                Color.fromRGBO(255, 230, 230, 1),
-                Color.fromRGBO(255, 255, 255, 1),
-              ],
-              begin: Alignment.bottomCenter,
-              end: Alignment.topCenter,
-              ),
-          
-            ),
-            
-            child:Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              
-               children:[
-                Center(
-                  child:Image.asset("assets/doctor_2.png") ,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromRGBO(253, 179, 179, 1),
+                    Color.fromRGBO(255, 189, 189, 1),
+                    Color.fromRGBO(250, 213, 213, 1),
+                    Color.fromRGBO(255, 230, 230, 1),
+                    Color.fromRGBO(255, 255, 255, 1),
+                  ],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
                 ),
-                const SizedBox(height: 95,),
-                
-                
-               Center(
-                
-                
-                 child: ElevatedButton(
-                          onPressed: () => Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => const LoginPage()) ),
-                              
-
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color.fromRGBO(51, 153, 255, 1),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(25),
-                            ),
-                            fixedSize: Size(MediaQuery.of(context).size.width*0.85,
-                            MediaQuery.of(context).size.height*0.08
-                            ),
-                            ),
-                          child: const Text("Sign in",
+              ),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: Image.asset("assets/doctor_2.png"),
+                    ),
+                    const SizedBox(
+                      height: 95,
+                    ),
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const LoginPage())),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromRGBO(51, 153, 255, 1),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          fixedSize: Size(
+                              MediaQuery.of(context).size.width * 0.85,
+                              MediaQuery.of(context).size.height * 0.08),
+                        ),
+                        child: const Text(
+                          "Sign in",
                           style: TextStyle(fontSize: 20),
-                          ),
-                          ),
-                          
-                          
-                  ),
-              const SizedBox(height: 15,),
-             Center(
-              
-                     child: ElevatedButton(
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Center(
+                      child: ElevatedButton(
                         onPressed: () => Navigator.push(
                             context,
                             MaterialPageRoute(
                                 builder: (context) => const RegisterPage2())),
                         style: ElevatedButton.styleFrom(
-                      
-                          backgroundColor: const Color.fromRGBO(51, 153, 255, 1),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25),
-                          ),
-                          //padding: EdgeInsets.symmetric(horizontal: 120, vertical: 20)
-                          fixedSize: Size(MediaQuery.of(context).size.width*0.85,
-                          MediaQuery.of(context).size.height*0.08
-                          )
+                            backgroundColor:
+                                const Color.fromRGBO(51, 153, 255, 1),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                            //padding: EdgeInsets.symmetric(horizontal: 120, vertical: 20)
+                            fixedSize: Size(
+                                MediaQuery.of(context).size.width * 0.85,
+                                MediaQuery.of(context).size.height * 0.08)),
+                        child: const Text(
+                          "Register",
+                          style: TextStyle(fontSize: 20),
                         ),
-                        child: const Text("Sign up",
-                      style: TextStyle(fontSize: 20),
-                        ),
-                        ),
-                                                     
-                   ),
-                   ]
-             
-            )
-            ),
-        )
-          );
+                      ),
+                    ),
+                  ])),
+        ));
   }
 }
