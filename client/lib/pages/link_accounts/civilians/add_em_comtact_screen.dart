d4dc2c04d22c45b83fb985fbe7a9e5bcd5f24f9a
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-03-10 19:24:29 +0530

    [Client] Change link to navigate to fr link page (for testing)

--- client/lib/pages/link_accounts/civilians/add_em_comtact_screen.dart
+++ client/lib/pages/link_accounts/civilians/add_em_comtact_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'package:client/pages/link_accounts/add_em_contact_screen2.dart';
+import 'package:client/pages/link_accounts/civilians/add_em_contact_screen2.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
--- client/lib/pages/main_screen.dart
+++ client/lib/pages/main_screen.dart
@@ -6,7 +6,7 @@ import 'report_incident_screen.dart';
 import 'package:client/pages/welcome_screen.dart';
 import 'package:client/storage/user_secure_storage.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
-import 'package:client/pages/link_accounts/civilians/link_account_home.dart';
+import 'package:client/pages/link_accounts/first_responders/link_accounts_home_fr.dart'; //change this
 
 class MainMenu extends StatefulWidget {
   const MainMenu({super.key});
