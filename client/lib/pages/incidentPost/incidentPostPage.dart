f8bc48beb46d918a042c84da4168028d38a7268d
Jayathu Shehan Dewmina <152476239+JayathuShehanDew@users.noreply.github.com>
2024-03-14 21:47:44 +0530

    working with incidentPostPage

--- client/lib/pages/incidentPost/incidentPostPage.dart
+++ client/lib/pages/incidentPost/incidentPostPage.dart
@@ -1,27 +1,47 @@
+// ignore: file_names
 import 'package:flutter/material.dart';
 
 class IncidentPostPage extends StatefulWidget {
   const IncidentPostPage({super.key});
 
   @override
-  IncidentPostPage createState() => IncidentPostPage();
+  State<IncidentPostPage> createState() => IncidentPost();
 }
 
-class incidentPage {
-  @override
+class IncidentPost extends State<IncidentPostPage> {
+  int counter = 0; // State variable to track the counter
+
+  // Function to increment the counter and update the UI
+  void _incrementCounter() {
+    setState(() {
+      counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: const Text(
-        'Incident Post',
-        style: TextStyle(
-          color: Color.fromARGB(255, 70, 70, 70),
-          fontSize: 30,
-          fontWeight: FontWeight.bold,
+      appBar: AppBar(
+        title: const Text('Stateful Widget Demo'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            ElevatedButton(
+              onPressed: _incrementCounter,
+              child: const Text('Increment'),
+            ),
+          ],
         ),
       ),
-      centerTitle: true,
-    ));
+    );
   }
 }
