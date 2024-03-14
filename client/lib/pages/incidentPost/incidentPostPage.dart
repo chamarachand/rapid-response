57869620631edfb671ecf642f50d17dc545340eb
Jayathu Shehan Dewmina <152476239+JayathuShehanDew@users.noreply.github.com>
2024-03-15 01:14:22 +0530

    add more things into incident post
    Please pull and check this if have any question about what i have do here
    please let me know

--- client/lib/pages/incidentPost/incidentPostPage.dart
+++ client/lib/pages/incidentPost/incidentPostPage.dart
@@ -9,39 +9,97 @@ class IncidentPostPage extends StatefulWidget {
 }
 
 class IncidentPost extends State<IncidentPostPage> {
-  int counter = 0; // State variable to track the counter
-
-  // Function to increment the counter and update the UI
-  void _incrementCounter() {
-    setState(() {
-      counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Stateful Widget Demo'),
+        title: const Text(
+          'Incidents',
+          style: TextStyle(
+            color: Color.fromARGB(255, 70, 70, 70),
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            ElevatedButton(
-              onPressed: _incrementCounter,
-              child: const Text('Increment'),
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              incidentTitle(),
+              incidentTitle(),
+              incidentTitle(),
+              incidentTitle(),
+              incidentTitle(),
+              incidentTitle(),
+              incidentTitle(),
+              incidentTitle(),
+              incidentTitle(),
+              incidentTitle(),
+              incidentTitle(),
+              incidentTitle(),
+              incidentTitle(),
+              incidentTitle()
+            ],
+          ),
         ),
       ),
     );
   }
+
+  Widget incidentTitle() => Container(
+        margin: const EdgeInsets.all(3.0),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              minimumSize: const Size.fromHeight(60.0),
+              backgroundColor: Colors.blueGrey,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0))),
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) => popupWindow(),
+            );
+          },
+          child: const Text('HELLO WORLD HELLO WORLD HELLO WORLD'),
+        ),
+      );
+
+  Widget popupWindow() => Container(
+        margin: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          // Add a border and shadow for visual clarity
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        padding: const EdgeInsets.all(20.0),
+        child: Stack(
+          children: [
+            const Text("THE INCIDENT POST GOES HERE"),
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                iconSize: 30.0,
+                icon: const Icon(
+                  Icons.close,
+                  color: Colors.grey,
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            )
+          ],
+        ),
+      );
 }
