3a7561c510ea364db1af83d619236ae603d62236
Jayathu Shehan Dewmina <152476239+JayathuShehanDew@users.noreply.github.com>
2024-03-16 04:26:32 +0530

    increased the appearance of the incident post page

--- client/lib/pages/incidentPost/incidentPostPage.dart
+++ client/lib/pages/incidentPost/incidentPostPage.dart
@@ -1,14 +1,16 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:client/pages/incidentPost/GmapOpen.dart';
 
 class IncidentPostPage extends StatefulWidget {
-  const IncidentPostPage({super.key});
+  const IncidentPostPage({Key? key}) : super(key: key);
 
   @override
-  State<IncidentPostPage> createState() => purpose();
+  State<IncidentPostPage> createState() => PurposeState();
 }
 
-class purpose extends State<IncidentPostPage> {
+class PurposeState extends State<IncidentPostPage> {
   double _currentSliderValue = 0.0;
 
   @override
@@ -58,106 +60,136 @@ class purpose extends State<IncidentPostPage> {
               minimumSize: const Size.fromHeight(60.0),
               backgroundColor: Colors.blueGrey,
               shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5.0))),
+                  borderRadius: BorderRadius.circular(10.0))),
           onPressed: () {
             showDialog(
               context: context,
-              builder: (BuildContext context) => popupWindow(),
+              builder: (BuildContext context) => popupWindow(context),
             );
           },
-          child: const Text('HELLO WORLD HELLO WORLD HELLO WORLD'),
+          child: const Text(
+            'HELLO WORLD HELLO WORLD HELLO WORLD',
+            style: TextStyle(color: Colors.white),
+          ),
         ),
       );
 
-  Widget popupWindow() => SingleChildScrollView(
-        child: Container(
-            margin: const EdgeInsets.all(8.0),
-            decoration: BoxDecoration(
-              // Add a border and shadow for visual clarity
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10.0),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 2,
-                  blurRadius: 5,
-                  offset: const Offset(0, 2),
+  Widget popupWindow(BuildContext context) => Dialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        backgroundColor:
+            Colors.transparent, // Make dialog background transparent
+        child: Stack(
+          children: [
+            // Blurred background
+            Positioned.fill(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                child: Container(
+                  color: const Color.fromARGB(255, 255, 255, 255)
+                      .withOpacity(1.0), // Adjust opacity as needed
                 ),
-              ],
+              ),
             ),
-            padding: const EdgeInsets.all(20.0),
-            child: contentInPopup()),
+            // Popup content
+            SingleChildScrollView(
+              child: Container(
+                margin: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color.fromARGB(255, 255, 255, 255)
+                          .withOpacity(0.5),
+                      blurRadius: 5,
+                    ),
+                  ],
+                ),
+                padding: const EdgeInsets.all(5.0),
+                child: contentInPopup(),
+              ),
+            ),
+          ],
+        ),
       );
 
   Widget contentInPopup() => Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Stack(
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Align(
-                alignment: Alignment.topLeft,
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        minimumSize: const Size(100, 40),
-                        backgroundColor: Colors.red),
-                    onPressed: () async {
-                      OpenGoogleMap openGoogleMap = OpenGoogleMap();
-                      openGoogleMap.launchGoogleMaps();
-                    },
-                    child: const Text("Location")),
-              ),
-              Align(
-                alignment: Alignment.topRight,
-                child: IconButton(
-                  iconSize: 30.0,
-                  icon: const Icon(
-                    Icons.close,
-                    color: Colors.grey,
-                  ),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(100, 40),
+                  backgroundColor: Colors.red,
                 ),
-              )
+                onPressed: () async {
+                  OpenGoogleMap openGoogleMap = OpenGoogleMap();
+                  openGoogleMap.launchGoogleMaps();
+                },
+                child: const Text("Location"),
+              ),
+              IconButton(
+                iconSize: 30.0,
+                icon: const Icon(
+                  Icons.close,
+                  color: Colors.grey,
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
             ],
           ),
           const SizedBox(height: 40.0),
           Container(
             height: 200,
-            child: Image.network(
-                'https://firebasestorage.googleapis.com/v0/b/rapid-response-802d3.appspot.com/o/sos_images%2F2024-03-13%2008%3A31%3A28.207333.jpg?alt=media&token=e0b09ff3-8583-48c2-9b08-9b3be9fb2ae6'),
+            decoration:
+                BoxDecoration(borderRadius: BorderRadius.circular(10.0)),
+            child: ClipRRect(
+              // Wrap the Image.network in a ClipRRect
+              borderRadius: BorderRadius.circular(10.0),
+              child: Image.network(
+                'https://downloadscdn6.freepik.com/188544/10/9871.jpg?filename=abstract-autumn-beauty-multi-colored-leaf-vein-pattern-generated-by-ai.jpg&token=exp=1710540628~hmac=942da11c44f9b7968addf796ff9c8e66',
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
           const SizedBox(height: 20.0),
           Container(
             height: 50,
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5.0),
-                color: const Color.fromARGB(255, 223, 223, 223)),
+              borderRadius: BorderRadius.circular(10.0),
+              color: const Color.fromARGB(255, 223, 223, 223),
+            ),
             child: Material(
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.play_arrow_rounded)),
-                  Listener(
-                    onPointerDown: (PointerDownEvent event) {},
-                    onPointerMove: (PointerMoveEvent event) {
-                      setState(() {
-                        _currentSliderValue = event.position.dx;
-                      });
-                    },
-                    onPointerUp: (PointerUpEvent event) {},
-                    child: Slider(
-                        value: _currentSliderValue,
-                        max: 100.0,
-                        divisions: 100,
-                        onChanged: (double value) {
-                          setState(() {
-                            _currentSliderValue = value;
-                          });
-                        }),
+                    onPressed: () {},
+                    icon: const Icon(Icons.play_arrow_rounded),
                   ),
-                  Text(" $_currentSliderValue"),
+                  Expanded(
+                    child: Slider(
+                      value: _currentSliderValue,
+                      max: 100.0,
+                      divisions: 100,
+                      onChanged: (double value) {
+                        setState(() {
+                          _currentSliderValue = value;
+                        });
+                      },
+                    ),
+                  ),
+                  Text(
+                    " $_currentSliderValue",
+                    style: TextStyle(color: Colors.black),
+                  ),
                 ],
               ),
             ),
@@ -166,12 +198,12 @@ class purpose extends State<IncidentPostPage> {
           Container(
             padding: const EdgeInsets.all(8.0),
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5.0),
-                border: Border.all(color: Colors.black),
-                color: const Color.fromARGB(255, 207, 207, 207)),
+              borderRadius: BorderRadius.circular(10.0),
+              color: const Color.fromARGB(255, 207, 207, 207),
+            ),
             child: const Text(
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-              style: TextStyle(color: Colors.black, fontSize: 19.0),
+              style: TextStyle(color: Colors.black, fontSize: 16.0),
               textAlign: TextAlign.justify,
             ),
           )
@@ -179,6 +211,8 @@ class purpose extends State<IncidentPostPage> {
       );
 }
 
-class color {
-  const color();
+void main() {
+  runApp(MaterialApp(
+    home: IncidentPostPage(),
+  ));
 }
