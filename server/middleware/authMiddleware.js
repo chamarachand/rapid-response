b13807f8a1282a8956de58737fd7148840bbc2e1
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-03-17 14:04:41 +0530

    [Client] pass jwtToken each time the search api is called. Succcfully validated at backend

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,15 +1,15 @@
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-  const { token } = req.header("x-auth-token");
+  const token = req.header("x-auth-token");
   if (!token) return res.status(401).send("Access denied. No token provided");
 
   try {
-    const decoded = jwt.verify(token, config.get("jwtPrivateKey")); // change this later
+    const decoded = jwt.verify(token, "jwtPrivateKey"); // change this later
     req.user = decoded;
     next();
   } catch (error) {
-    res.status.send("Invalid token");
+    res.status(401).send("Invalid token");
   }
 };
 
