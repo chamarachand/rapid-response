d0b207c50c66d5ff78f25a5cbdd943b6d1beeadb
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-03-19 10:21:59 +0530

    [Server] Create route in firstResponderRoutes.js to remove a supervisee

--- server/routes/firstResponderRoutes.js
+++ server/routes/firstResponderRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const { FirstResponder, validate } = require("../models/first-responder");
 const authMiddleware = require("../middleware/authMiddleware");
+const { mongo, default: mongoose } = require("mongoose");
 
 // Get
 router.get("/", (req, res) => {
@@ -55,5 +56,53 @@ router.post("/", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
+
+//Delete
+router.delete(
+  "/remove/supervisee/:superviseeId",
+  authMiddleware,
+  async (req, res) => {
+    const userId = req.user.id;
+    const { superviseeId } = req.params;
+
+    if (!userId || !superviseeId) return res.status(400).send("Bad Request");
+
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+      const removeSupervisee = await FirstResponder.updateOne(
+        { _id: userId },
+        { $pull: { superviseeAccounts: superviseeId } },
+        { session }
+      );
+
+      const removeSupervisor = await FirstResponder.updateOne(
+        { _id: superviseeId },
+        { $pull: { supervisorAccounts: userId } },
+        { session }
+      );
+
+      if (
+        removeSupervisee.modifiedCount === 0 ||
+        removeSupervisor.modifiedCount === 0
+      ) {
+        await session.abortTransaction();
+        session.endSession();
+        return res
+          .status(404)
+          .send("Supervisee account with the given id not found for the user");
+      }
+
+      await session.commitTransaction();
+      session.endSession();
+
+      return res.status(200).send("Emergency contact removed successfully");
+    } catch (error) {
+      console.log("Error: " + error);
+      return res.status(500).send("Internal Server Error");
+    }
+  }
+);
 
 module.exports = router;
