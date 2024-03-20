8af895ff7185f42567b35873e8b87f1cf8ac27d2
Navidu <navidubc@gmail.com>
2024-03-20 20:11:34 +0530

    create incident_post backend

--- server/routes/sosRoute.js
+++ server/routes/sosRoute.js
@@ -1,15 +1,15 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const sosReportSchema = new mongoose.Schema({
-  id: { type: String, required: true },
-  image: { type: String },
-  voice: { type: String },
-  emergencyType: { type: String, required: true },
-  location: { type: Object, required: true },
-  date: {type: String, required: true}, // Assuming location is a GeoJSON object
-});
+// const sosReportSchema = new mongoose.Schema({
+//   id: { type: String, required: true },
+//   image: { type: String },
+//   voice: { type: String },
+//   emergencyType: { type: String, required: true },
+//   location: { type: Object, required: true },
+//   date: {type: String, required: true}, // Assuming location is a GeoJSON object
+// });
 
-const SOSReport = mongoose.model("SOSReport", sosReportSchema);
+// const SOSReport = mongoose.model("SOSReport", sosReportSchema);
 
 const express = require("express");
 const router = express.Router();
