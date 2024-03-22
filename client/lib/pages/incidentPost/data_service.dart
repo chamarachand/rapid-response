5bbbabb61a5b7d4391ad5b8a35039dacd10c4870
Navidu <navidubc@gmail.com>
2024-03-22 09:24:00 +0530

    change server ip

--- client/lib/pages/incidentPost/data_service.dart
+++ client/lib/pages/incidentPost/data_service.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 
 
 Future<List<dynamic>> fetchPostData() async {
-  final response = await http.get(Uri.parse('http://localhost:3000/api/posts/incidents'));
+  final response = await http.get(Uri.parse('http://10.0.2.2:3000/api/posts/incidents'));
   if (response.statusCode == 200) {
     return json.decode(response.body);
   } else {
@@ -15,7 +15,7 @@ Future<List<dynamic>> fetchPostData() async {
 
 
 Future<List<dynamic>> fetchSosData() async {
-  final response = await http.get(Uri.parse('http://localhost:3000/api/posts/sos'));
+  final response = await http.get(Uri.parse('http://10.0.2.2:3000/api/posts/sos'));
   if (response.statusCode == 200) {
     return json.decode(response.body);
   } else {
