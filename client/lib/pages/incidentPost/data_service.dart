471b12333464940ab6a0e724c36baf0359398292
Navidu <navidubc@gmail.com>
2024-03-22 08:12:48 +0530

    create fetch data function

--- client/lib/pages/incidentPost/data_service.dart
+++ client/lib/pages/incidentPost/data_service.dart
@@ -1,13 +1,24 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-/*
-Future  <List<dynamic>> fetchData() async* {
-  final response = await http.get(Uri.parse('http://  :3000/api/incidentPost'));
-  if(response.statusCode == 200){
+
+
+Future<List<dynamic>> fetchPostData() async {
+  final response = await http.get(Uri.parse('http://localhost:3000/api/posts/incidents'));
+  if (response.statusCode == 200) {
     return json.decode(response.body);
-  }else{
-    throw Exception('Failed to load data.');
+  } else {
+    throw Exception('Failed to load data');
   }
 }
 
-*/
+
+
+
+Future<List<dynamic>> fetchSosData() async {
+  final response = await http.get(Uri.parse('http://localhost:3000/api/posts/sos'));
+  if (response.statusCode == 200) {
+    return json.decode(response.body);
+  } else {
+    throw Exception('Failed to load data');
+  }
+}
