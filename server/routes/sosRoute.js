cde45376e4e757a6f93305c532858e04d046cfb9
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2024-03-23 07:58:08 +0530

    [Server] Completely moved sos model to the seperate file in models dir

--- server/routes/sosRoute.js
+++ server/routes/sosRoute.js
@@ -1,18 +1,6 @@
-// const mongoose = require("mongoose");
-
-// const sosReportSchema = new mongoose.Schema({
-//   id: { type: String, required: true },
-//   image: { type: String },
-//   voice: { type: String },
-//   emergencyType: { type: String, required: true },
-//   location: { type: Object, required: true },
-//   date: {type: String, required: true}, // Assuming location is a GeoJSON object
-// });
-
-// const SOSReport = mongoose.model("SOSReport", sosReportSchema);
-
 const express = require("express");
 const router = express.Router();
+const { SOSReport } = require("../models/sosReport");
 
 router.post("/", async (req, res) => {
   console.log("Reached");
@@ -25,7 +13,7 @@ router.post("/", async (req, res) => {
       voice,
       emergencyType,
       location,
-      date
+      date,
     });
 
     await newSOSReport.save();
